8ab7e4b07c06ff915aed78d84ae6c2b59271cc6e
RifqiAfandi <rifqitriafandi.20@gmail.com>
2025-03-14 01:18:43 +0700

    feat: models and migration

---
+++ migrations/20250313175511-create-kos-mahasiswa.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Kos_Mahasiswas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      phone_number: {
+        type: Sequelize.INTEGER
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      facility: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Kos_Mahasiswas');
+  }
+};
---
+++ models/kos_pegawai.js
@@ -0,0 +1,41 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Kos_Pegawai extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Kos_Pegawai.init(
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+      gender: {
+        type: DataTypes.ENUM("Putra", "Putri"),
+        defaultValue: "Putra",
+        validate: {
+          isIn: {
+            args: [["Putra", "Putri"]],
+            msg: "Gender Tidak Ditemukan",
+          },
+        },
+        allowNull: false,
+      },
+      description: { type: DataTypes.STRING, allowNull: false },
+      phone_number: { type: DataTypes.INTEGER, allowNull: false },
+      city: { type: DataTypes.STRING, allowNull: false },
+      price: { type: DataTypes.BIGINT, allowNull: false },
+      facility: { type: DataTypes.STRING, allowNull: false },
+      kosImg: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Kos_Pegawai",
+    }
+  );
+  return Kos_Pegawai;
+};
