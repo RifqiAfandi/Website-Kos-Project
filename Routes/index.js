61a27312c92e0094262655a91ecdf09f3507d221
RifqiAfandi <rifqitriafandi.20@gmail.com>
2025-03-18 22:20:01 +0700

    feat: add health check, on lost and on error route

--- Routes/index.js
+++ Routes/index.js
@@ -5,6 +5,7 @@ const Kos_Pegawai = require("./kosPegawaiRoute");
 const Kos_Premium = require("./kosPremiumRoute");
 const Cities = require("./citiesRoute");
 const News = require("./newsRoute");
+const systemController = require("../controllers/systemController");
 
 router.use("/kos-mahasiswa", Kos_Mahasiswa);
 router.use("/kos-pegawai", Kos_Pegawai);
@@ -12,6 +13,8 @@ router.use("/kos-premium", Kos_Premium);
 router.use("/cities", Cities);
 router.use("/news", News);
 
-router.use("/health-check");
+router.use("/health-check", systemController.healthCheck);
+router.use(systemController.onLost);
+router.use(systemController.onError);
 
 module.exports = router;
