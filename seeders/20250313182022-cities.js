2488ca0d6eeb829772315e813d18738129c9288b
Rifqi Afandi <rifqitriafandi.20@gmail.com>
2025-03-21 21:03:10 +0700

    feat: add cities seeders

--- seeders/20250313182022-cities.js
+++ seeders/20250313182022-cities.js
@@ -6,10 +6,66 @@ module.exports = {
     await queryInterface.bulkInsert(
       "Cities",
       [
-        {
-          name: "John Doe",
-          isBetaMember: false,
+        { 
+          name: 'Jakarta', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Jakarta.jpg?updatedAt=1742564626064', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
         },
+        { 
+          name: 'Surabaya', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Surabaya.jpg?updatedAt=1742564621801', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
+        },
+        { 
+          name: 'Bandung', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Bandung.jpg?updatedAt=1742564623349', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
+        },
+        { 
+          name: 'Yogyakarta', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Yogyakarta.jpg?updatedAt=1742564621825', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
+        },
+        { 
+          name: 'Medan', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Medan.jpg?updatedAt=1742564622138', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
+        },
+        { 
+          name: 'Denpasar', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Denpasar.jpg?updatedAt=1742564623864', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
+        },
+        { 
+          name: 'Semarang', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Semarang.jpg?updatedAt=1742564624261', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
+        },
+        { 
+          name: 'Makassar', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Makassar.jpg?updatedAt=1742564622637', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
+        },
+        { 
+          name: 'Depok', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Depok.jpg?updatedAt=1742565078863', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
+        },
+        { 
+          name: 'Malang', 
+          cityImg: 'https://ik.imagekit.io/RifqiAfandi/Malang.jpg?updatedAt=1742564624413', 
+          createdAt: new Date(), 
+          updatedAt: new Date() 
+        }
       ],
       {}
     );
