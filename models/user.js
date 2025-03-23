9c66076b1f26f7be48c2525ccdc2beb598e2515f
Rifqi Afandi <rifqitriafandi.20@gmail.com>
2025-03-23 13:29:14 +0700

    refactor: validate user model

--- models/user.js
+++ models/user.js
@@ -14,11 +14,81 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    name: DataTypes.STRING,
-    phone: DataTypes.INTEGER,
-    role: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Invalid email format'
+        },
+        notNull: {
+          args: true,
+          msg: 'Email is required'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Email is required'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Password is required'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Password is required'
+        },
+        len: {
+          args: [6],
+          msg: 'Password at least 6 characters'
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Name is required'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Name is required'
+        }
+      }
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Phone is required'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Phone is required'
+        }
+      }
+    },
+    role: {
+      type: DataTypes.ENUM('admin', 'user'),
+      defaultValue: "user",
+      validate: {
+        isIn: {
+          args: [['admin', 'user']],
+          msg: 'Role must be admin or user'
+        }
+      }
+    },
     profileImg: DataTypes.TEXT
   }, {
     sequelize,
