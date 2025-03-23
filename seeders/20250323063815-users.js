4c0e06fa606021542c36712db95d1645043c1c3d
Rifqi Afandi <rifqitriafandi.20@gmail.com>
2025-03-23 13:55:25 +0700

    feat: add user seeders

--- seeders/20250323063815-users.js
+++ seeders/20250323063815-users.js
@@ -5,13 +5,26 @@ const bcrypt = require("bcrypt");
 module.exports = {
   async up(queryInterface, Sequelize) {
     const hashedPassword = await bcrypt.hash("alhamdulillah", 10);
-    const profilUrl = "https://ik.imagekit.io/RifqiAfandi/profileImg.png?updatedAt=1742712498677";
+    const profilUrl =
+      "https://ik.imagekit.io/RifqiAfandi/profileImg.png?updatedAt=1742712498677";
     await queryInterface.bulkInsert(
       "Users",
       [
         {
-          name: "John Doe",
-          isBetaMember: false,
+          email: "admin@example.com",
+          password: hashedPassword,
+          name: "Admin",
+          phone: 1234567890,
+          role: "admin",
+          profileImg: profilUrl,
+        },
+        {
+          email: "user@example.com",
+          password: hashedPassword,
+          name: "Vicky",
+          phone: 9876543210,
+          role: "user",
+          profileImg: profilUrl,
         },
       ],
       {}
