710baaa9b97c37582fb4cd3b548b38775e4fcdd0
Rifqi Afandi <rifqitriafandi.20@gmail.com>
2025-03-23 13:24:24 +0700

    refactor: change migration table name

--- migrations/20250313175542-create-kos-pegawai.js
+++ migrations/20250313175542-create-kos-pegawai.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Kos_Pegawais', {
+    await queryInterface.createTable('Kos_Pegawai', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -41,6 +41,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Kos_Pegawais');
+    await queryInterface.dropTable('Kos_Pegawai');
   }
 };
--- migrations/20250313175602-create-kos-premium.js
+++ migrations/20250313175602-create-kos-premium.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Kos_Premia', {
+    await queryInterface.createTable('Kos_Premium', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -41,6 +41,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Kos_Premia');
+    await queryInterface.dropTable('Kos_Premium');
   }
 };
