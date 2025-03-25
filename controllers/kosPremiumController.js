62f21d6605d54187f92e76d9d0ec9607969d544a
Rifqi Afandi <rifqitriafandi.20@gmail.com>
2025-03-25 23:28:57 +0700

    feat: add create kos premium

--- controllers/kosPremiumController.js
+++ controllers/kosPremiumController.js
@@ -1,4 +1,5 @@
 const { Kos_Premiums } = require("../models");
+const imagekit = require("../lib/imagekit");
 
 async function getAllKosPremium(req, res) {
   try {
@@ -99,7 +100,93 @@ async function getKosPremiumById(req, res) {
   }
 }
 
+async function createKosPremium(req, res) {
+  try {
+    if (req.file) {
+      const file = req.file;
+      const split = file.originalname.split(".");
+      const ext = split[split.length - 1];
+      const { name, gender, description, phone_number, city, price } =
+        req.body;
+
+      const updloadImg = await imagekit.upload({
+        file: file.bufefer,
+        fileName: `${split[0]}-${Date.now()}.${ext}`,
+      });
+      if (!updloadImg) {
+        return res.status(500).json({
+          status: "Failed",
+          message: "Failed to upload image",
+          isSuccess: false,
+          data: null,
+        });
+      } else if (updloadImg) {
+        const newPremium = await Kos_Premiums.create({
+          name,
+          gender,
+          description,
+          phone_number,
+          city,
+          price,
+          kosImg: updloadImg.url,
+        });
+
+        res.status(201).json({
+          status: "Success",
+          message: "Success create kos premium",
+          isSuccess: true,
+          data: {
+            newPremium,
+          },
+        });
+      } else {
+        const newPremium = await Kos_Premiums.create({
+          name,
+          gender,
+          description,
+          phone_number,
+          city,
+          price,
+        });
+        res.status(201).json({
+          status: "Success",
+          message: "Success create kos premium",
+          isSuccess: true,
+          data: {
+            newPremium,
+          },
+        });
+      }
+    }
+  } catch (error) {
+    if (error.name === "SequelizeValidationError") {
+      const errorMessage = error.errors.map((err) => err.message);
+      return res.status(400).json({
+        status: "Failed",
+        message: errorMessage[0],
+        isSuccess: false,
+        data: null,
+      });
+    } else if (error.name === "SequelizeDatabaseError") {
+      return res.status(400).json({
+        status: "Failed",
+        message: error.message || "Database error",
+        isSuccess: false,
+        data: null,
+      });
+    } else {
+      return res.status(500).json({
+        status: "Failed",
+        message: error.message,
+        isSuccess: false,
+        data: null,
+      });
+    }
+  }
+}
+
 module.exports = {
   getAllKosPremium,
   getKosPremiumById,
+  createKosPremium,
 };
